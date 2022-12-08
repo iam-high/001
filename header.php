841c97a37a51e22ac85e3258f5ecee31d2fed21e
ravi <ravisankar.s@tendercuts.in>
2022-12-08 23:46:28 +0530

    php configured

---
+++ header.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HIGH LABS</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/images/favicon/site.html">
+
+    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+    <!-- preloader - start -->
+    <div class="preloader">
+        <div id="pulse-wrapper">
+            <div id="pulse">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+        <img src="assets/logo/high-labs-logo-square.png" alt="slope-logo">
+    </div>
+    <!-- preloader - end -->
+
+    <!-- navigation - start -->
+    <div class="navigation-wrapper">
+        <div class="placeholder"></div>   
+        <nav class="navigation enabled-sticky">
+            <div class="navigation-bar">
+                <div class="logo">
+                    <a 
+                    href="index-2.html" 
+                    class="custom-logo-link" 
+                    rel="home" 
+                    aria-current="page">
+                        <img width="150" height="75" src="assets/logo/high-labs-logo.png" class="custom-logo" alt="Slope"></a>
+                </div>
+                <div class="hamburger ">
+                    <a href="#">
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                    </a>
+                </div>
+            </div>
+            <div class="navigation-menu">
+                <div class="navigation-menu-image">
+                    <img 
+                        width="683" 
+                        height="1037" 
+                        src="assets/images/navigation_menu_image.png" 
+                        alt="navigation image" 
+                    >
+                </div>
+                <div class="navigation-menu-menu">
+                    <div class="navigation-menu-menu--wrapper">
+                        <ul class="menu">
+                            <li class="menu-item menu-item-has-children">
+                                <a href="index-2.html">Home</a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item">
+                                        <a href="index-2.html">Home 1</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="index-3.html">Home 2</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="menu-item menu-item-has-children">
+                                <a href="#">Pages</a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item">
+                                        <a href="about.html">About</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="contact.html">Contact</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="404.html">404</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="menu-item menu-item-has-children">
+                                <a href="services-1.html">Services</a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item">
+                                        <a href="services-1.html">Service Style 1</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="services-2.html">Services Style 2</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="service-detailed.html">Service Detailed</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="menu-item menu-item-has-children">
+                                <a href="projects-1.html">Projects</a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item">
+                                        <a href="projects-1.html">Projects Style 1</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="projects-2.html">Projects Style 2</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="project-detailed.html">Project Detailed</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="menu-item menu-item-has-children">
+                                <a href="blog-grid.html">Blog</a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item">
+                                        <a href="blog-grid.html">Blog Grid</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="blog-list.html">Blog List</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="blog-detailed.html">Post Detailed</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="navigation-menu-social">
+                        <h6>FOLLOW</h6>
+                        <ul>
+                            <li>
+                                <a href="https://www.facebook.com/gfx.partner">
+                                    <i class="fab fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/gfxpartner">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.pinterest.com/gfxpartner">
+                                    <i class="fab fa-pinterest"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.behance.net/gfxpartner">
+                                    <i class="fab fa-behance"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <!-- navigation - end -->
